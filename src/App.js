d4260777314928fce4241369f856a55c3fb39303
salgu1998 <salgu1998@gmail.com>
2021-01-04 20:04:59 +0900

    state 사용

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import Subject from "./components/Subject";
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      subject:{title:'WEB', sub:'world wide web!!!'},
+      subject2:{title:'React', sub:'This is React!!!'},
+      content:{title:'HTML', desc:'HTML is HyperText Markup Language.'}
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!!!"></Subject>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         <TOC></TOC>
-        <Subject title="React" sub="This is React!!!"></Subject>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject title={this.state.subject2.title} sub={this.state.subject2.sub}></Subject>
+        <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
       </div>
     );
   }
