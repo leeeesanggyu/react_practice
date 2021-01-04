84fa65344532b6ab628502cbd2603963973ab66f
salgu1998 <salgu1998@gmail.com>
2021-01-04 21:22:45 +0900

    state, key 추가

--- src/App.js
+++ src/App.js
@@ -10,14 +10,20 @@ class App extends Component {
     this.state={
       subject:{title:'WEB', sub:'world wide web!!!'},
       subject2:{title:'React', sub:'This is React!!!'},
-      content:{title:'HTML', desc:'HTML is HyperText Markup Language.'}
+      content:{title:'HTML', desc:'HTML is HyperText Markup Language.'},
+      toc:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+        {id:4, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
   render() {
     return (
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.toc}></TOC>
         <Subject title={this.state.subject2.title} sub={this.state.subject2.sub}></Subject>
         <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
       </div>
